96d81bc999c2a836f22eb1be1a78e31637bd78e7
Dev12122002 <devoza121220@gmail.com>
2022-03-09 17:57:54 +0530

    make indentations proper in user class

--- Electronics/src/main/java/Users/User.java
+++ Electronics/src/main/java/Users/User.java
@@ -23,67 +23,66 @@ public class User {
         this.setPassword(password);
     }
 	
-	
-	
 	public int getUserId() {
 		return userId;
 	}
+	
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
 	
-	
 	public String getUserName() {
 		return userName;
 	}
+	
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 	
-	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
 	
-	
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
+	
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 	
-	
 	public String getDateOfBirth() {
 		return dateOfBirth;
 	}
+	
 	public void setDateOfBirth(String dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
 	
-	
 	public String getRole() {
 		return role;
 	}
+	
 	public void setRole(String role) {
 		this.role = role;
 	}
 	
-	
 	public Address getBilling() {
 		return billing;
 	}
+	
 	public void setBilling(Address billing) {
 		this.billing = billing;
 	}
 	
-	
 	public Address getShipping() {
 		return shipping;
 	}
+	
 	public void setShipping(Address shipping) {
 		this.shipping = shipping;
 	}
@@ -96,6 +95,4 @@ public class User {
 		this.password = password;
 	}
 	
-	
-
 }
